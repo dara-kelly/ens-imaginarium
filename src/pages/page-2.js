1a21a8164eeb46d8f615e9206377da6e527ac881
dara-kelly <56123923+dara-kelly@users.noreply.github.com>
2020-12-15 11:31:11 +0000

    Update page-2.js
    Adding insubstantial waffle to test HTML

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,10 +6,10 @@ import SEO from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="Other page" />
+    <h1>How can there be an extension of nothing&#63;</h1>
+    <p>This is the second page, or another page, the non-first page. It is defined only by its opposition to the first page (which is to say, spatially for you, temporally for me), and has no substance.</p>
+    <Link to="/">Go back. You can. But it won&#39;t be the same.</Link>
   </Layout>
 )
 
