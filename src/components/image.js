93dd62b180504c5b6021c02600ffb06e0c5b9648
dara-kelly <56123923+dara-kelly@users.noreply.github.com>
2020-12-15 12:48:15 +0000

    Update image.js
    LHO

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "eb8edd37795b961678b6e618166d9a33f5adff64.jpg" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
