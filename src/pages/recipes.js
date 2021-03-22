0c7a630e2f009ef85d2c2e687ecf7bddee7a647e
dara-kelly <56123923+dara-kelly@users.noreply.github.com>
2021-03-22 12:25:37 +0000

    Update recipes.js

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,1 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
 
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="Other page" />
+    <h1>"Recipes page"</h1>
+    <p>"Recipes will be going in below"</p>
+    <Link to="/">Go back. You can. But it won&#39;t be the same.</Link>
+  </Layout>
+)
+
+export default SecondPage
