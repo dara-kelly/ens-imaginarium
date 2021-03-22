7607347e53facdbf728bba07fe8be24f6e94668e
dara-kelly <56123923+dara-kelly@users.noreply.github.com>
2021-03-22 12:31:08 +0000

    Update recipes.js

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -6,14 +6,14 @@ import Layout from "../components/layout"
 
 const recipePage = () => (
   <Layout>
-    <h1>"Recipes page"</h1>
-    <p>"Recipes will be going in below"</p>
-  <li>"Bread" </li>
-  <li>"Coconut and turmeric chickpea stew" </li>
-  <li>"Vegan curry" </li>
-  <li>"Leeks and preserved lemons"</li>
-  <li>"Shakshuka" </li>
-  <li>"Mezze" </li>
+    <h1>Recipes page</h1>
+    <p>Recipes will be going in below</p>
+  <li>Bread </li>
+  <li>Coconut and turmeric chickpea stew </li>
+  <li>Vegan curry </li>
+  <li>Leeks and preserved lemons</li>
+  <li>Shakshuka </li>
+  <li>Mezze </li>
  
     <Link to="/">Go back. You can. But it won&#39;t be the same.</Link>
   </Layout>
