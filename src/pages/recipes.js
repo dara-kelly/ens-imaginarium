b107502d69aae33613768646ab7cc212af1fc802
dara-kelly <56123923+dara-kelly@users.noreply.github.com>
2021-04-13 16:46:22 +0100

    Update recipes.js

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -15,10 +15,10 @@ const recipePage = () => (
   <li>Shakshuka </li>
   <li>Mezze </li>
  
-  <div style="color: grey"; margin:5rem>
+
    <p> Language is external in the sense both of the word of others, the highest point of which seems to be cliche, and the exact opposite, in the lyrical moment which seems to puncture the smooth surface of the theyself and reveal in its singularity a strange universality. Most of our living and our language occurs stranded between these two points, one of pure muttering which we try to escape, a grunting, barely legible <i>stimmung </i></p>
   <p> The other, absolute singularity, which is true generality. The general truth, the shared truth, can only be encountered as an individual, and felt here as a moment of sublimity, and nausea. The voice of the 'theyself' is precisely that of individuality from the outside, which is to say a mediated or subtracted individuality, one which is reduced and fearful, which senses its own internal void and sees the eyes gazing at it, fearing that they must sense it too. </p>
-  </div>
+
     <Link to="/">Go back. You can. But it won&#39;t be the same.</Link>
   </Layout>
 )
