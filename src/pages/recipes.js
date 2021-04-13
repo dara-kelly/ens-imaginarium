925eaf2130348845e84c9bc72a9efd312deb6854
dara-kelly <56123923+dara-kelly@users.noreply.github.com>
2021-04-13 16:49:50 +0100

    Update recipes.js

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -18,7 +18,7 @@ const recipePage = () => (
 
    <p> Language is external in the sense both of the word of others, the highest point of which seems to be cliche, and the exact opposite, in the lyrical moment which seems to puncture the smooth surface of the theyself and reveal in its singularity a strange universality. Most of our living and our language occurs stranded between these two points, one of pure muttering which we try to escape, a grunting, barely legible <i>stimmung </i></p>
   <p> The other, absolute singularity, which is true generality. The general truth, the shared truth, can only be encountered as an individual, and felt here as a moment of sublimity, and nausea. The voice of the 'theyself' is precisely that of individuality from the outside, which is to say a mediated or subtracted individuality, one which is reduced and fearful, which senses its own internal void and sees the eyes gazing at it, fearing that they must sense it too. </p>
-
+  <p Why does she go home? Nothing left in the city; homegoing duplicates the movement to the hills, with a distinction in focus - place vs. space, history vs. topology. History must begin as her history, she must escape generality first. To begin, she is trapped in generality, and Brately in a decentred individualism, a non-individualism. Neither of these can take part in, or understand, history.</p>
     <Link to="/">Go back. You can. But it won&#39;t be the same.</Link>
   </Layout>
 )
