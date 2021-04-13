70bcd74ab0a21e8451bfa1297df4b554f0051625
dara-kelly <56123923+dara-kelly@users.noreply.github.com>
2021-04-13 16:52:59 +0100

    Update layout.js
    removed font import

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,6 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
-import "@fontsource/vt323"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
