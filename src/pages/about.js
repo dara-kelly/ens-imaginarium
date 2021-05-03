b9ae92958f52c9faf72dc4cdcb6a34d6e5f638cc
Dara Kelly <dazk98@gmail.com>
2021-05-03 15:34:57 +0100

    fixed about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from "gatsby";
+import Layout from "../components/layout";
 
-export default class About extends Component {
-	render() {
-		return (
-			<div>
-			<h1>About</h1>
-			<p>Dara Kelly is a writer living in Dublin</p>
-			</div>
-		)
-	}
-};
+const aboutPage = () => (
+	<Layout>		
+
+	<h1>About</h1>
+		<p>Dara Kelly is a writer living in Dublin. This website collects some of his thoughts, which would otherwise hide in recesses and plot against him.</p>
+
+	</Layout>
+export default aboutPage
