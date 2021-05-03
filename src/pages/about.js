5946580e87c53cf1a43023da1c2231117f2c4a55
Dara Kelly <dazk98@gmail.com>
2021-05-03 16:36:47 +0100

    changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react';
-import { Link } from "gatsby";
 import Layout from "../components/layout";
 
 const aboutPage = () => (
-	
 	<Layout>		
-
+	<main>
 	<h1>About</h1>
-		
 	<p>Dara Kelly is a writer living in Dublin. This website collects some of his thoughts, which would otherwise hide in recesses and plot against him.</p>
-
+</main>
 	</Layout>
 )
 export default aboutPage
