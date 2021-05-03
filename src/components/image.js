1785cabf44771e93af5e480cfc3733bdddfb2143
Dara Kelly <dazk98@gmail.com>
2021-05-03 13:12:41 +0100

    castrochavez added

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "eb8edd37795b961678b6e618166d9a33f5adff64.jpg" }) {
+      placeholderImage: file(relativePath: { eq: "castrochavezry.jpeg" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
