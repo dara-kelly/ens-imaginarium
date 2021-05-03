e320b20fe7aaf4a62fb41d632605f1a9d106a885
Dara Kelly <dazk98@gmail.com>
2021-05-03 15:35:36 +0100

    add readinglist.js

---
+++ src/pages/readinglist.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+
+const recipePage = () => (
+  <Layout>
+    <h1>Reading list</h1>
+ 
+    <ul> 
+	<li>Joan Retallack - The Poethical Wager</li>
+    	<li> Donald Davie - Articulating Energy</li>
+    	<li> Hugh Kenner - The Pound Era</li>
+    	<li> Guy Davenport - Geographies of the Imagination</li>
+    	<li> Frank Kermode - Romantic Image</li>
+    	<li> Marjorie Perloff - 21st Century Modernism</li> 
+    	<li> Anne Carson - Eros the Bittersweet</li>
+    </ul>
+export default recipePage
