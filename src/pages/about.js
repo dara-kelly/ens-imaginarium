87463bb90dc07ef626356b91f939ff04eeeb6491
Dara Kelly <dazk98@gmail.com>
2021-05-03 16:21:18 +0100

    test

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,10 +3,12 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 
 const aboutPage = () => (
+	
 	<Layout>		
 
 	<h1>About</h1>
-		<p>Dara Kelly is a writer living in Dublin. This website collects some of his thoughts, which would otherwise hide in recesses and plot against him.</p>
+		
+	<p>Dara Kelly is a writer living in Dublin. This website collects some of his thoughts, which would otherwise hide in recesses and plot against him.</p>
 
 	</Layout>
 )
---
+++ src/pages/recipes2.js
@@ -0,0 +1,26 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+
+const recipePage = () => (
+  <Layout>
+    <h1>Recipes page</h1>
+    <p>Recipes will be going in below</p>
+  <li>Bread </li>
+  <li>Coconut and turmeric chickpea stew </li>
+  <li>Vegan curry </li>
+  <li>Leeks and preserved lemons</li>
+  <li>Shakshuka </li>
+  <li>Mezze </li>
+ 
+
+  <p> Language is external in the sense both of the word of others, the highest point of which seems to be cliche, and the exact opposite, in the lyrical moment which seems to puncture the smooth surface of the theyself and reveal in its singularity a strange universality. Most of our living and our language occurs stranded between these two points, one of pure muttering which we try to escape, a grunting, barely legible <i>stimmung </i> </p>
+  <p> The other, absolute singularity, which is true generality. The general truth, the shared truth, can only be encountered as an individual, and felt here as a moment of sublimity, and nausea. The voice of the 'theyself' is precisely that of individuality from the outside, which is to say a mediated or subtracted individuality, one which is reduced and fearful, which senses its own internal void and sees the eyes gazing at it, fearing that they must sense it too. </p>
+  <p> Why does she go home? Nothing left in the city; homegoing duplicates the movement to the hills, with a distinction in focus - place vs. space, history vs. topology. History must begin as her history, she must escape generality first. To begin, she is trapped in generality, and Brately in a decentred individualism, a non-individualism. Neither of these can take part in, or understand, history.</p>
+    <Link to="/">Go back. You can. But it won&#39;t be the same.</Link>
+  </Layout>
+)
+
+export default recipePage
