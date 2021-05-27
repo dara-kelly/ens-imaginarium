5c2b0dfb179075c754dd984dbb20118c47cb5417
Dara Kelly <dazk98@gmail.com>
2021-05-27 17:34:14 +0100

    reversed changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import Tx from "../components/image"
 
 const IndexPage = () => (
   <Layout>
@@ -13,7 +12,7 @@ const IndexPage = () => (
     <p>You look on helplessly while your mind insists on perceiving</p>
     <p>You wish you could stop. <i>You cannot</i>.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Tx />
+      <Image />
     </div>
     <Link to="/page-2/">You may go on, but not come back</Link>
     
