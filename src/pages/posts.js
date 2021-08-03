021b8ac19401e1214fbdcb6c32a28c6dd770c27e
Dara Kelly <dazk98@gmail.com>
2021-08-03 22:35:45 +0100

    newposts

---
+++ src/pages/posts.js
@@ -0,0 +1,17 @@
+
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+
+const postsPage = () => (
+
+  <h1>Posts sorted by date</h1>
+    <ul>
+      <li>01-Jan-21</li>
+      <li>08-Jan-21 </li>
+    </ul>
+
+
+export default postsPage
